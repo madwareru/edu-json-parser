b2fe9a16c0f780383b77793e4e6767da2e496092
madwareru <madware.ru@gmail.com>
2020-04-02 09:32:00 +0300

    In fact, we don't need any "StringPretender" struct, as long as we can just use a &'static str hack

--- edu-json-parser/examples/unsafe_lab.rs
+++ edu-json-parser/examples/unsafe_lab.rs
@@ -1,43 +1,14 @@
-use std::{str, slice};
-use std::ops::Deref;
+use std::{str};
 
-pub struct StringPretender {
-    len: usize,
-    internal_ptr: *const u8
-}
-
-impl StringPretender {
-    pub fn from_raw_ptr_and_len(ptr: *const u8, len: usize) -> Self {
-        StringPretender{ internal_ptr: ptr, len }
-    }
-
-    pub fn as_str(&self) -> &str {
-        unsafe {
-            str::from_utf8_unchecked(
-                slice::from_raw_parts(self.internal_ptr, self.len)
-            )
-        }
-    }
-}
-
-impl Deref for StringPretender {
-    type Target = str;
-
-    #[inline(always)]
-    fn deref(&self) -> &Self::Target {
-        self.as_str()
-    }
-}
-
-pub struct StringPretenderAllocator {
+pub struct StringAllocator {
     buf: String,
     prev_buffers: Vec<String>,
     current_offset: usize
 }
 
-impl StringPretenderAllocator {
+impl StringAllocator {
     pub fn new() -> Self {
-        StringPretenderAllocator {
+        StringAllocator {
             buf: String::with_capacity(4096),
             prev_buffers: Vec::new(),
             current_offset: 0
@@ -45,7 +16,7 @@ impl StringPretenderAllocator {
     }
 
     pub fn with_capacity(cap: usize) -> Self {
-        StringPretenderAllocator {
+        StringAllocator {
             buf: String::with_capacity(cap),
             prev_buffers: Vec::new(),
             current_offset: 0
@@ -55,7 +26,7 @@ impl StringPretenderAllocator {
     // You should be 100 percent sure that f(&mut s) will not grow capacity of an s.
     // In other case, you are in a situation of a panic.
     // So, be sure that certain_len_in_bytes calculated right
-    pub fn allocate_string<F>(&mut self, certain_len_in_bytes: usize, mut f: F) -> StringPretender
+    pub unsafe fn allocate_string<F>(&mut self, certain_len_in_bytes: usize, mut f: F) -> &'static str
         where F: FnMut(&mut String) -> ()
     {
         if self.current_offset + certain_len_in_bytes > self.buf.capacity() {
@@ -67,21 +38,20 @@ impl StringPretenderAllocator {
             self.prev_buffers.push(old_buffer);
             self.current_offset = 0;
         }
-        let old_cap = self.buf.capacity();
-        f(&mut self.buf);
-        if self.buf.len() - self.current_offset > certain_len_in_bytes {
-            panic!("certain_len_in_bytes is lower than it should be!")
-        }
-        if self.buf.capacity() > old_cap {
-            panic!("unexpected grow of capacity")
-        }
-        let pretender = unsafe {
-            let ptr = self.buf.as_mut_ptr();
-            let part: *mut u8 = ptr.add(self.current_offset);
-            StringPretender::from_raw_ptr_and_len(part, self.buf.len() - self.current_offset)
+        let interned = {
+            let start = self.current_offset;
+            let old_cap = self.buf.capacity();
+            f(&mut self.buf);
+            if self.buf.len() - self.current_offset > certain_len_in_bytes {
+                panic!("certain_len_in_bytes is lower than it should be!")
+            }
+            if self.buf.capacity() > old_cap {
+                panic!("unexpected grow of capacity")
+            }
+            &self.buf[start..self.buf.len()]
         };
         self.current_offset = self.buf.len();
-        pretender
+        &*(interned as *const str)
     }
 }
 
@@ -89,19 +59,21 @@ const HELLO: &'static str = "Hello, ";
 const WORLD: &'static str = "World!";
 
 fn main() {
-    let mut pretender_allocator = StringPretenderAllocator::with_capacity(16);
-    let certain_size = HELLO.len() + WORLD.len();
-    let hw = pretender_allocator.allocate_string(certain_size, |s: &mut String|{
-        s.push_str(HELLO);
-        s.push_str(WORLD);
-    });
-    let why_are_you_doing_this = pretender_allocator.allocate_string(32, |s: &mut String|{
-        s.push_str("to be or not to be?");
-    });
-    let why_are_you_doing_this2 = pretender_allocator.allocate_string(48, |s: &mut String|{
-        s.push_str("lorum ipsum dolor sit amet ktulhu ftagn");
-    });
-    println!("String content: {}", hw.deref());
-    println!("String content: {}", why_are_you_doing_this.deref());
-    println!("String content: {}", why_are_you_doing_this2.deref());
+    unsafe {
+        let mut pretender_allocator = StringAllocator::with_capacity(16);
+        let certain_size = HELLO.len() + WORLD.len();
+        let hw = pretender_allocator.allocate_string(certain_size, |s: &mut String|{
+            s.push_str(HELLO);
+            s.push_str(WORLD);
+        });
+        let why_are_you_doing_this = pretender_allocator.allocate_string(32, |s: &mut String|{
+            s.push_str("to be or not to be?");
+        });
+        let why_are_you_doing_this2 = pretender_allocator.allocate_string(48, |s: &mut String|{
+            s.push_str("lorum ipsum dolor sit amet ktulhu ftagn");
+        });
+        println!("String content: {}", hw);
+        println!("String content: {}", why_are_you_doing_this);
+        println!("String content: {}", why_are_you_doing_this2);
+    }
 }
