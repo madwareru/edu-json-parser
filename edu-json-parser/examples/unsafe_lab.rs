fbf6ee5047486d1fb3538c2d6cd68c1d6f42ba81
madwareru <madware.ru@gmail.com>
2020-04-02 10:09:25 +0300

    Interner from https://matklad.github.io/2020/03/22/fast-simple-rust-interner.html

--- edu-json-parser/examples/unsafe_lab.rs
+++ edu-json-parser/examples/unsafe_lab.rs
@@ -1,4 +1,5 @@
 use std::{str};
+use std::collections::HashMap;
 
 pub struct StringAllocator {
     buf: String,
@@ -26,6 +27,8 @@ impl StringAllocator {
     // You should be 100 percent sure that f(&mut s) will not grow capacity of an s.
     // In other case, you are in a situation of a panic.
     // So, be sure that certain_len_in_bytes calculated right
+    // It could be higher than actually needed length anyway,
+    // but in this case you might take non optimal memory consumption
     pub unsafe fn allocate_string<F>(&mut self, certain_len_in_bytes: usize, mut f: F) -> &'static str
         where F: FnMut(&mut String) -> ()
     {
@@ -38,7 +41,7 @@ impl StringAllocator {
             self.prev_buffers.push(old_buffer);
             self.current_offset = 0;
         }
-        let interned = {
+        let str_slice = {
             let start = self.current_offset;
             let old_cap = self.buf.capacity();
             f(&mut self.buf);
@@ -51,7 +54,45 @@ impl StringAllocator {
             &self.buf[start..self.buf.len()]
         };
         self.current_offset = self.buf.len();
-        &*(interned as *const str)
+        &*(str_slice as *const str)
+    }
+}
+
+pub struct Interner {
+    map: HashMap<&'static str, u32>,
+    vec: Vec<&'static str>,
+    allocator: StringAllocator
+}
+
+impl Interner {
+    pub fn with_capacity(cap: usize) -> Interner {
+        let cap = cap.next_power_of_two();
+        Interner {
+            map: HashMap::default(),
+            vec: Vec::new(),
+            allocator: StringAllocator::with_capacity(cap)
+        }
+    }
+
+    pub fn intern(&mut self, name: &str) -> u32 {
+        if let Some(&id) = self.map.get(name) {
+            return id;
+        }
+        let name = unsafe {
+            self.allocator.allocate_string(name.len(), |s: &mut String| s.push_str(name))
+        };
+        let id = self.map.len() as u32;
+        self.map.insert(name, id);
+        self.vec.push(name);
+
+        debug_assert!(self.lookup(id) == name);
+        debug_assert!(self.intern(name) == id);
+
+        id
+    }
+
+    pub fn lookup(&self, id: u32) -> &str {
+        self.vec[id as usize]
     }
 }
 
@@ -76,4 +117,25 @@ fn main() {
         println!("String content: {}", why_are_you_doing_this);
         println!("String content: {}", why_are_you_doing_this2);
     }
+
+    let mut interner = Interner::with_capacity(32);
+    let for_interned = interner.intern("for");
+    let while_interned = interner.intern("while");
+    let repeat_interned = interner.intern("repeat");
+    let until_interned = interner.intern("until");
+    let reinterpret_interned = interner.intern("reinterpret");
+    let cast_interned = interner.intern("cast");
+    let repeat_duplicate = interner.intern("repeat");
+
+    println!(
+        "Interners are: {}, {}, {}, {}, {}, {}, {}",
+        for_interned,
+        while_interned,
+        repeat_interned,
+        until_interned,
+        reinterpret_interned,
+        cast_interned,
+        repeat_duplicate
+    );
+
 }
