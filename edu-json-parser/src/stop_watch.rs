f70b8bf5f5762d9a6013eea6b0c1a3230de0660d
madwareru <madware.ru@gmail.com>
2020-04-07 21:01:27 +0300

    Helper macro

--- edu-json-parser/src/stop_watch.rs
+++ edu-json-parser/src/stop_watch.rs
@@ -19,3 +19,28 @@ impl Drop for StopWatch {
         println!("\"{}\"\t{}", self.associated_name, self.time_started.elapsed().as_nanos());
     }
 }
+
+#[macro_export]
+macro_rules! with_time_measuring {
+    {
+        named_as: $sw_name:literal measures: $block:block
+    } => {
+        let _sw = StopWatch::start($sw_name);
+        $block
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::stop_watch::StopWatch;
+    #[test]
+    fn test_measure_time() {
+        with_time_measuring! (
+            named_as: "wow!" measures: {
+                let x = 1;
+                let y = 1;
+                x + y
+            }
+        );
+    }
+}
